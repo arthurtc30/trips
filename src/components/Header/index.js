a02838382ce38ca6d61f2fcbf5c091926e6ebb04
Arthur Teixeira <52799942+arthurtc30@users.noreply.github.com>
2021-12-06 09:29:05 -0300

    header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import './style.css';
+
+export default function Header() {
+ return (
+   <div className="container">
+     <Link to="/">
+        <img className="logo" src={logo} alt="Trips project logo" />
+      </Link>
+
+      <Link to="/reservas" className="reserva">
+        <div>
+          <strong>My bookings</strong>
+          <span>0 bookings</span>
+        </div>
+      </Link>
+   </div>
+ );
+}
